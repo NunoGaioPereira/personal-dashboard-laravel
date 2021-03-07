8695bbb4fcd418f0ab18efad0fdfb749dccbadbd
Nuno Pereira <nunocgpereira@gmail.com>
2021-03-07 13:35:54 +0000

    Style tracker card

--- resources/views/livewire/tracker-counter.blade.php
+++ resources/views/livewire/tracker-counter.blade.php
@@ -1,8 +1,12 @@
-<div class="bg-white p-10 shadow w-1/4">
-    <button wire:click="decrement" class="p-3 bg-blue-600 text-blue-50">-</button>
-    <h2>{{ $this->tracker->title }}</h2>
-    <h2>{{ $this->tracker->value }}</h2>
+<div class="bg-white p-10 shadow max-w-sm rounded-lg">
+    <h2 class="font-bold text-2xl text-gray-900">{{ $this->tracker->title }}</h2>
+    <p>{{ $this->tracker->description }}</p>
+    
     <!-- <form wire:submit.prevent="increment"> -->
-        <button wire:click="increment" class="p-3 bg-blue-600 text-blue-50">+</button>
+    <div class="justify-between w-full">
+        <button wire:click="decrement" class="w-8 h-8 font-bold bg-black text-white rounded-md">-</button>
+        <h2 class="inline-block">{{ $this->tracker->value }}</h2>
+        <button wire:click="increment" class="w-8 h-8 font-bold bg-black text-white rounded-md">+</button>
+    </div>
     <!-- </form> -->
 </div>
