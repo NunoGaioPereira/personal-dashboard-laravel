ba95f000b0f12dfc33e238f3def99b3e326e9b5e
Nuno Pereira <nunocgpereira@gmail.com>
2021-03-07 13:25:50 +0000

    Working on dashboard sidenav

---
+++ resources/views/dashboard/dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+  <div class="flex container mx-auto">
+    <div class="w-full mx-auto my-8 px-4">
+      <div class="w-full bg-white rounded p-8 sm:p-16">
+        <h1 class="text-2xl font-medium mb-2">Welcome to your Personal Dashboard</h1>
+        <h2 class="font-medium text-gray-600 tracking-wide">Here you can manage your account, update your profile and ask for support</h2>
+      </div>
+    </div>
+  </div>
+@endsection
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -8,11 +8,70 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title')</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
     
-    <!-- Styles -->
+    <style>
+    /* html, body {
+        background-color: #fff;
+        color: #1a1a1a;
+        font-family: 'Montserrat', sans-serif;
+        font-weight: 200;
+        height: 100vh;
+        margin: 0;
+    }
+    .full-height {
+        height: 100vh;
+    }
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+    .position-ref {
+        position: relative;
+    }
+    .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+    }
+    .content {
+        text-align: center;
+    }
+    .title {
+        font-size: 84px;
+    }
+    .links > a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 13px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+    .m-b-md {
+        margin-bottom: 30px;
+    } */
+    .d-link {
+        /* background-color: #212121; */
+        color: #4d4d4d;
+    }
+    .d-link:hover {
+        background-color: #292929;
+    }
+    .d-link.active {
+        background-color: #292929;
+        color: #ccc;
+    }
+    .dsec-link {
+        background-color: #212121;
+        color: #fff;
+        font-size: 14px;
+    }
+    .dsec-link:hover {
+        background-color: #292929;
+    }
+</style>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
     @yield('headers')
@@ -20,8 +79,16 @@
 </head>
 <body class="bg-gray-100 h-screen antialiased leading-none font-sans">
     <div id="app">
-        @yield('content')
+        @include('partials.dashboard_sidenav')
+
+        <div class="bg-main-bg-gray min-h-screen p-6" style="margin-left: 50px;">
+            @yield('content')
+        </div>
+
     </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>    
+    <script src="{{ asset('js/app.js') }}" defer></script>
     @yield('scripts')
     @livewireScripts
 </body>
